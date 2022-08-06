1eac8f39e177c1c4f1815d8f1e63d3b55286f06c
Malfrador <malfrador@gmail.com>
2022-08-06 20:20:33 +0200

    Register passive spell events again

--- SpellbookSpells/src/main/java/de/erethon/spellbook/spells/PassiveSpell.java
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/spells/PassiveSpell.java
@@ -1,16 +1,19 @@
 package de.erethon.spellbook.spells;
 
+import de.erethon.spellbook.Spellbook;
 import de.erethon.spellbook.api.SpellData;
 import de.erethon.spellbook.api.SpellbookSpell;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 
 public abstract class PassiveSpell extends SpellbookSpell implements Listener {
 
     public PassiveSpell(LivingEntity caster, SpellData spellData) {
         super(caster, spellData);
+        Plugin implementer = Spellbook.getInstance().getImplementer();
         keepAliveTicks = -1;
-        //data.getSpellbook().getServer().getPluginManager().registerEvents(this, data.getSpellbook().getImplementingPlugin());
+        implementer.getServer().getPluginManager().registerEvents(this, implementer);
     }
 
 }
