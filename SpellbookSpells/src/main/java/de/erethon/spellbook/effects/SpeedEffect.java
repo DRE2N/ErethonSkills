129bc2c91b328db5534d0240102fa9eab2b5792a
Malfrador <malfrador@gmail.com>
2022-08-06 23:48:12 +0200

    Add speed effect

---
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/effects/SpeedEffect.java
@@ -0,0 +1,29 @@
+package de.erethon.spellbook.effects;
+
+import de.erethon.spellbook.api.EffectData;
+import de.erethon.spellbook.api.SpellEffect;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.LivingEntity;
+
+public class SpeedEffect extends SpellEffect {
+
+    AttributeInstance instance;
+    double value = 0.0;
+
+    public SpeedEffect(EffectData data, LivingEntity target, int duration, int stacks) {
+        super(data, target, duration, stacks);
+        instance = target.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+    }
+
+    @Override
+    public void onApply() {
+        value = data.getDouble("bonus", 1.0);
+        instance.setBaseValue(instance.getBaseValue() + value);
+    }
+
+    @Override
+    public void onRemove() {
+        instance.setBaseValue(instance.getBaseValue() - value);
+    }
+}
