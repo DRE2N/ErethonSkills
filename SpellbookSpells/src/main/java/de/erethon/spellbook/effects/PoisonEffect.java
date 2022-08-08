bbfd14364f8a78399877c3f07325d6e050e3194e
Malfrador <malfrador@gmail.com>
2022-08-08 20:50:40 +0200

    Add poisoned weapon

---
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/effects/PoisonEffect.java
@@ -0,0 +1,24 @@
+package de.erethon.spellbook.effects;
+
+import de.erethon.papyrus.DamageType;
+import de.erethon.spellbook.api.EffectData;
+import de.erethon.spellbook.api.SpellEffect;
+import org.bukkit.entity.LivingEntity;
+
+public class PoisonEffect extends SpellEffect {
+
+    private int tick = 0;
+
+    public PoisonEffect(EffectData data, LivingEntity caster, LivingEntity target, int duration, int stacks) {
+        super(data, caster, target, duration, stacks);
+    }
+
+    @Override
+    public void onTick() {
+        tick++;
+        if (tick >= 20) {
+            tick = 0;
+            target.damage(data.getDouble("damage", 1.0), DamageType.MAGIC);
+        }
+    }
+}
---
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/spells/assassin/PoisonWeapon.java
@@ -0,0 +1,30 @@
+package de.erethon.spellbook.spells.assassin;
+
+import de.erethon.papyrus.DamageType;
+import de.erethon.spellbook.api.EffectData;
+import de.erethon.spellbook.api.SpellCaster;
+import de.erethon.spellbook.api.SpellData;
+import de.erethon.spellbook.api.SpellEffect;
+import de.erethon.spellbook.api.SpellbookSpell;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
+
+public class PoisonWeapon extends SpellbookSpell {
+
+    int count = data.getInt("count", 3);
+    EffectData effectData = Bukkit.getServer().getSpellbookAPI().getLibrary().getEffectByID("Poison");
+
+    public PoisonWeapon(LivingEntity caster, SpellData spellData) {
+        super(caster, spellData);
+        keepAliveTicks = data.getInt("duration", 200);
+    }
+
+    @Override
+    public double onAttack(SpellCaster target, double damage, DamageType type) {
+        if (count > 0) {
+            count--;
+            target.addEffect(caster, effectData, 1, 1);
+        }
+        return super.onAttack(target, damage, type);
+    }
+}
