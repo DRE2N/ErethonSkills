8f848bc8d393e52f1e61463a1837e06e3121622d
Malfrador <malfrador@gmail.com>
2022-08-08 21:10:18 +0200

    Add assassin stacking attack

---
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/spells/assassin/StackingAttack.java
@@ -0,0 +1,43 @@
+package de.erethon.spellbook.spells.assassin;
+
+import de.erethon.papyrus.DamageType;
+import de.erethon.spellbook.api.SpellCaster;
+import de.erethon.spellbook.api.SpellData;
+import de.erethon.spellbook.api.SpellbookSpell;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+
+public class StackingAttack extends SpellbookSpell {
+
+    LivingEntity target;
+    int stacks;
+
+    public StackingAttack(LivingEntity caster, SpellData spellData) {
+        super(caster, spellData);
+        keepAliveTicks = spellData.getInt("duration", 10) * 20;
+    }
+
+    @Override
+    protected boolean onPrecast() {
+        Entity selected = caster.getTargetEntity(data.getInt("range", 10));
+        if (selected instanceof LivingEntity entity) {
+            target = entity;
+            return AssassinUtils.hasEnergy(caster, data);
+        } else {
+            caster.sendActionbar("<color:#ff0000>Kein gültiges Ziel!");
+            return false;
+        }
+    }
+
+    @Override
+    public double onAttack(SpellCaster target, double damage, DamageType type) {
+        if (target != this.target) {
+            return damage;
+        }
+        damage = damage + (data.getDouble("bonusDamagePerStack", 1.0) * stacks);
+        if (stacks <= data.getInt("maxStacks", 7)) {
+            stacks++;
+        }
+        return super.onAttack(target, damage, type);
+    }
+}
