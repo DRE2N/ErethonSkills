4bc752296e753fe7151892005640ee47484b1afa
Malfrador <malfrador@gmail.com>
2023-05-31 03:06:37 +0200

    Add a status display for the pet, make it controllable

--- SpellbookSpells/src/main/java/de/erethon/spellbook/spells/ranger/pet/RangerPet.java
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/spells/ranger/pet/RangerPet.java
@@ -1,6 +1,7 @@
 package de.erethon.spellbook.spells.ranger.pet;
 
-import de.erethon.bedrock.chat.MessageUtil;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
@@ -9,23 +10,34 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.animal.Wolf;
-import net.minecraft.world.level.Level;
+import org.bukkit.Color;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_19_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_19_R3.entity.CraftLivingEntity;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.TextDisplay;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.function.Predicate;
+import org.joml.AxisAngle4f;
+import org.joml.Vector3f;
 
 public class RangerPet extends Wolf {
 
+    private final FollowOwnerGoal followOwnerGoal = new FollowOwnerGoal(this, 1.0D, 2.0F, 10.0F, false);
+    private final Transformation petStatusTextTransformation = new Transformation(new Vector3f(0, 1.1f, 0), new AxisAngle4f(0, 0, 0, 0), new Vector3f(0.5f, 0.5f, 0.5f), new AxisAngle4f(0, 0, 0, 0));
+
     private EntityType<?> petDisplayType = EntityType.COW;
     private final LivingEntity owner;
+    private final org.bukkit.entity.Mob bukkitMob = getBukkitMob();
+    private TextDisplay statusDisplay;
+
+    private boolean shouldAttackAutomatically = true;
+    private boolean isCurrentlyGoingToLocation = false;
 
     public RangerPet(org.bukkit.entity.LivingEntity bukkitOwner, World world, org.bukkit.entity.EntityType craftDisplayType) {
         super(EntityType.WOLF, ((CraftWorld) world).getHandle());
@@ -34,9 +46,18 @@ public class RangerPet extends Wolf {
         setPetDisplayType(craftDisplayType);
         setTame(true);
         setOwnerUUID(owner.getUUID());
-        goalSelector.removeAllGoals(Predicate.not((x -> false)));
-        goalSelector.addGoal(0, new FollowOwnerGoal(this, 1.0D, 2.0F, 10.0F, false));
-        goalSelector.addGoal(1, new MeleeAttackGoal(this, 2.0D, true));
+        setCustomName(owner.getName());
+        setSilent(true);
+        goalSelector.removeAllGoals(goal -> true);
+        goalSelector.addGoal(9, followOwnerGoal);
+        goalSelector.addGoal(0, new MeleeAttackGoal(this, 2.0D, true));
+        statusDisplay = world.spawn(bukkitOwner.getLocation(), TextDisplay.class, display -> {
+            display.setTransformation(petStatusTextTransformation);
+            display.text(Component.empty());
+            display.setAlignment(TextDisplay.TextAligment.CENTER);
+            display.setBillboard(Display.Billboard.VERTICAL);
+            display.setBackgroundColor(Color.fromARGB(0, 1, 1, 1));
+        });
     }
 
     @Override
@@ -47,6 +68,7 @@ public class RangerPet extends Wolf {
     public void setScaledAttributes(double attributeMultiplier) {
         for (Attribute attribute : BuiltInRegistries.ATTRIBUTE.stream().toList()) {
             if (attribute == Attributes.MOVEMENT_SPEED) continue; // We don't want to make the pet extremely slow
+            if (attribute == Attributes.ATTACK_SPEED) continue; // We don't want to make the pet attack extremely slow either
             if (getAttribute(attribute) == null || owner.getAttribute(attribute) == null) continue;
             getAttribute(attribute).setBaseValue(owner.getAttribute(attribute).getBaseValue() * attributeMultiplier);
         }
@@ -56,7 +78,13 @@ public class RangerPet extends Wolf {
     public void makeAttack(org.bukkit.entity.LivingEntity bukkitLiving) {
         CraftLivingEntity craftLiving = (CraftLivingEntity) bukkitLiving;
         LivingEntity living = craftLiving.getHandle();
-        setTarget(living);
+        setTarget(living, EntityTargetEvent.TargetReason.CUSTOM, false);
+        statusDisplay.text(Component.text("ATTACKING").color(NamedTextColor.RED));
+    }
+
+    public void goToLocation(int x, int y, int z) {
+        goalSelector.removeGoal(followOwnerGoal);
+        moveControl.setWantedPosition(x, y, z, 1.0D);
     }
 
     public void setPetDisplayType(org.bukkit.entity.EntityType type) {
@@ -70,5 +98,22 @@ public class RangerPet extends Wolf {
 
     public void addToWorld() {
         level.addFreshEntity(this);
+        bukkitMob.addPassenger(statusDisplay);
+    }
+
+    public boolean isShouldAttackAutomatically() {
+        return shouldAttackAutomatically;
+    }
+
+    public boolean isCurrentlyGoingToLocation() {
+        return isCurrentlyGoingToLocation;
+    }
+
+    public void setShouldAttackAutomatically(boolean shouldAttackAutomatically) {
+        this.shouldAttackAutomatically = shouldAttackAutomatically;
+    }
+
+    public void setCurrentlyGoingToLocation(boolean currentlyGoingToLocation) {
+        isCurrentlyGoingToLocation = currentlyGoingToLocation;
     }
 }
