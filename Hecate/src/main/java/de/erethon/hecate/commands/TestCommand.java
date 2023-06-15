1ad116403898acb340b4356d17a49cca4dcbf7ed
Malfrador <malfrador@gmail.com>
2023-06-15 18:59:55 +0200

    testcommand

--- Hecate/src/main/java/de/erethon/hecate/commands/TestCommand.java
+++ Hecate/src/main/java/de/erethon/hecate/commands/TestCommand.java
@@ -2,31 +2,15 @@ package de.erethon.hecate.commands;
 
 import de.erethon.bedrock.command.ECommand;
 import de.erethon.hecate.Hecate;
-import de.erethon.spellbook.spells.ranger.pet.RangerPet;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
-import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
-import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.decoration.ItemFrame;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.MapItem;
-import net.minecraft.world.level.Level;
-import org.bukkit.Location;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_19_R3.CraftWorld;
-import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class TestCommand extends ECommand {
 
@@ -45,12 +29,15 @@ public class TestCommand extends ECommand {
     @Override
     public void onExecute(String[] args, CommandSender commandSender) {
         Hecate plugin = Hecate.getInstance();
+        ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
+        ItemMeta meta = item.getItemMeta();
+        meta.displayName(MiniMessage.miniMessage().deserialize("<red>\"Test\""));
+        meta.addEnchant(Enchantment.DURABILITY, 1, true);
+        item.setItemMeta(meta);
         Player player = (Player) commandSender;
-        RangerPet pet = new RangerPet(player, player.getWorld(), org.bukkit.entity.EntityType.COW);
-        pet.teleport(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ());
-        pet.addToWorld();
-        pet.setScaledAttributes(0.5);
+        player.sendMessage(item.displayName());
     }
+
 
 
 
