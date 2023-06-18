567f923b87a7df230ffbdb0cbe3d6cbd654d968d
Malfrador <malfrador@gmail.com>
2023-06-18 20:11:31 +0200

    Empty out TestCommand

--- Hecate/src/main/java/de/erethon/hecate/commands/TestCommand.java
+++ Hecate/src/main/java/de/erethon/hecate/commands/TestCommand.java
@@ -1,16 +1,7 @@
 package de.erethon.hecate.commands;
 
 import de.erethon.bedrock.command.ECommand;
-import de.erethon.hecate.Hecate;
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Mob;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class TestCommand extends ECommand {
 
@@ -28,14 +19,6 @@ public class TestCommand extends ECommand {
 
     @Override
     public void onExecute(String[] args, CommandSender commandSender) {
-        Hecate plugin = Hecate.getInstance();
-        ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
-        ItemMeta meta = item.getItemMeta();
-        meta.displayName(MiniMessage.miniMessage().deserialize("<red>\"Test\""));
-        meta.addEnchant(Enchantment.DURABILITY, 1, true);
-        item.setItemMeta(meta);
-        Player player = (Player) commandSender;
-        player.sendMessage(item.displayName());
     }
 
 
