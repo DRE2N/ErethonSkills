d66eb884a7bf2abf801a8a542b7677cdfed825e7
Malfrador <malfrador@gmail.com>
2023-06-20 01:58:44 +0200

    Fixup Firesword

--- SpellbookSpells/src/main/java/de/erethon/spellbook/spells/warrior/FireSword.java
+++ SpellbookSpells/src/main/java/de/erethon/spellbook/spells/warrior/FireSword.java
@@ -3,25 +3,26 @@ package de.erethon.spellbook.spells.warrior;
 import de.erethon.papyrus.DamageType;
 import de.erethon.spellbook.api.SpellData;
 import de.erethon.spellbook.api.SpellbookSpell;
+import org.bukkit.Color;
+import org.bukkit.Particle;
 import org.bukkit.entity.LivingEntity;
 
 public class FireSword extends SpellbookSpell {
 
-    protected int duration = 200;
-    protected int BonusDamage= 10;
+    private final int bonusDamage = data.getInt("bonusDamage", 10);
 
     public FireSword(LivingEntity caster, SpellData spellData) {
         super(caster, spellData);
-        keepAliveTicks = duration;
+        keepAliveTicks = data.getInt("duration", 200);
     }
-    public double onDamage(LivingEntity target, double damage, DamageType type) {
+
+    @Override
+    public double onAttack(LivingEntity target, double damage, DamageType type) {
         if (type == DamageType.MAGIC) {
             return super.onDamage(target, damage, type);
         }
-        target.damage(BonusDamage, DamageType.MAGIC);
-        return super.onDamage(target, damage + BonusDamage, type);
+        target.damage(damage + bonusDamage, caster, DamageType.MAGIC);
+        target.getWorld().spawnParticle(Particle.REDSTONE, target.getLocation().add(0, 1, 0), 1, new Particle.DustOptions(Color.ORANGE,3f));
+        return 0; // We can't deal damage twice in the same attack.
     }
 }
-
-
-
