a39041799ec8d889284e0a70ac1ac5d232a95580
Malfrador <malfrador@gmail.com>
2025-08-08 22:35:06 +0200

    Add Hecate command alias

--- Hecate/src/main/java/de/erethon/hecate/commands/HecateCommandCache.java
+++ Hecate/src/main/java/de/erethon/hecate/commands/HecateCommandCache.java
@@ -3,13 +3,17 @@ package de.erethon.hecate.commands;
 import de.erethon.bedrock.command.ECommandCache;
 import de.erethon.bedrock.plugin.EPlugin;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HecateCommandCache extends ECommandCache {
     public static final String LABEL = "hecate";
+    private static final Set<String> ALIASES = Set.of("h", "hecate", "hec");
 
     EPlugin plugin;
 
     public HecateCommandCache(EPlugin plugin) {
-        super(LABEL, plugin);
+        super(LABEL, plugin, ALIASES, new HashSet<>());
         this.plugin = plugin;
         addCommand(new SkillCommand());
         addCommand(new DisplayCommand());
